3e9ac5e82fced3b6068d4c2cea84b83c22bcdabb
Sathviks2 <sathviks3465@gmail.com>
2022-05-14 10:17:19 +0530

    p8

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,13 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+filename = input("dataset/mbox-short.txt")
+try:
+    fname=open("dataset/mbox-short.txt")
+except:
+    print("File name cannot be opened",filename)
+    exit()
+count=0
+for line in fname:
+    if line.startswith("X-DSPAM-Confidence:"):
+        count=count+1
+        print('there where',count,'X-DSPAM-Confidence',fname)
