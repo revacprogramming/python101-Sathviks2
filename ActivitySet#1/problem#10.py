d4dd7e4efe2555b9fb454120f0cc94840b61ad6b
Sathviks2 <sathviks3465@gmail.com>
2022-05-17 21:28:51 +0530

    p10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,26 @@
 # Dictionaries
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+lst = list()
 
-filename = "dataset/mbox-short.txt"
+for line in handle:
+    if line.startswith("From"):
+        line = line.split()
+        if line[0]=="From":
+            lst.append(line[1])
+
+counts = dict()
+for word in lst:
+    counts[word] = counts.get(word,0)+1
+
+bigcount = None
+bigword = None
+for word,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigcount = count
+        bigword = word
+
+print(bigword,bigcount)
+
