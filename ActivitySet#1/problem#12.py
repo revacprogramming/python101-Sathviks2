eef42a12af2483ca7ff8f420770f9ba09688f903
Sathviks2 <sathviks3465@gmail.com>
2022-05-23 22:18:53 +0530

    p12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,3 +1,10 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
-
+import re
+file = open('regex_sum_232111.txt', 'r')
+sum = 0
+for line in file:
+    numbers = re.findall('[0-9]+', line)
+    for number in numbers:
+        sum = sum + int(number)
+        print(sum)
